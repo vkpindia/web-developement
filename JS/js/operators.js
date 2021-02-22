984823c633c0db7a763825eb6e53881bfac1df01
vkpindia <infovkpg@gmail.com>
2021-02-22 08:46:20 +0530

    commit

---
+++ JS/js/operators.js
@@ -0,0 +1,62 @@
+/** 
+ * In JavaScript , an operator is a special seymbol used to perform 
+ * operstions on operands(values and variables) 
+ * 
+ * For example 2 + 3 (2 and 3 are the operands and + is the operator)
+ * 
+ * Types of operators
+ * 1. Assignment Operators
+ * 2. Arithmetic operators
+ * 3. Comparison operators
+ * 4. Logical operators
+ * 5. Bitwise operators
+ * 6. String Operators
+ * 7. Others Operators(Ternary, Spread etc.)
+ * */
+
+/** 1. Assignment Operators
+ * Assignment operators used to assign values to variables, Fro example
+ * const x = 5;
+ * 
+ * 
+ */
+
+
+ //= assignment
+ let a=7; 
+ console.log('a assignment', a); //7
+
+ //+= addition assignment
+
+a += 5; // a = a + 5
+
+console.log('addition assignment:', a); //12
+
+// -= Subtraction Assignment
+
+a -= 10; // a = a-10;
+
+console.log('Subtraction Assignment:', a); // 2
+
+// *= Multiplication Assignment
+
+a *= 10; // a = a * 10
+
+console.log('Multiplication Assignment:', a); // 20
+
+// /= Division Assignment
+
+a /= 2; // a = a / 2;
+
+console.log('Division Assignment:', a); // 10
+
+// %= Remainder Assignment
+
+//a %= 1; // a = a % 3;
+
+console.log('Remainder Assignment:', a); // 1
+
+// **= Exponential Assignment
+
+a **= 2; // a = a**2; 
+console.log('Exponential Assignment:', a);
